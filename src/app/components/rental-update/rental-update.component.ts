ba0d456c3b253c1c1a08784cd41f3dd8c008d77a
nagydani98 <36605345+nagydani98@users.noreply.github.com>
2020-05-08 14:38:16 +0200

    started rental closure

--- src/app/components/rental-update/rental-update.component.ts
+++ src/app/components/rental-update/rental-update.component.ts
@@ -4,6 +4,7 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ApiService} from '../../service/api.service';
 import {Rental} from '../../model/rental';
+import {Vehicle} from '../../model/vehicle';
 
 @Component({
   selector: 'app-rental-update',
@@ -14,6 +15,9 @@ export class RentalUpdateComponent implements OnInit {
   submitted = false;
   closureForm: FormGroup;
   rentalData: Rental;
+  rentedVehicles: Vehicle[];
+  VehicleStatus: any = ['AVAILABLE', 'RENTED', 'RETIRED'];
+  bill: number;
 
   constructor(
     public fb: FormBuilder,
@@ -25,9 +29,11 @@ export class RentalUpdateComponent implements OnInit {
   ngOnInit() {
     const id = this.actRoute.snapshot.paramMap.get('id');
     this.getRental(id);
+    this.getRentedVehicles();
     this.closureForm = this.fb.group({
       damage: ['', [Validators.required]],
       miles: ['', [Validators.required, Validators.pattern('^[^0][0-9]*$')]],
+      duration: [''],
       bill: ['', [Validators.required]]
     });
   }
@@ -43,15 +49,43 @@ export class RentalUpdateComponent implements OnInit {
     });
   }
 
+
+  calculateBill() {
+    this.bill = 0;
+    const rentalDuration = this.rentalData.start.getDay() - this.rentalData.end.getDay();
+    console.log('Rental duration: ' + rentalDuration);
+    for (const vehicle of this.rentedVehicles) {
+        this.bill += rentalDuration * vehicle.rentalFee;
+    }
+    if (this.closureForm.value.damage) {
+      this.bill += 100000;
+      console.log('Vehicle damaged! Extra fee added as penalty!');
+    }
+  }
+
+  getRentedVehicles() {
+    for (const vId of this.rentalData.RentedVehicle) {
+      this.apiService.getVehicle(vId).subscribe(data => {
+        this.rentedVehicles.push(data);
+      });
+    }
+  }
+
   onSubmit() {
     this.submitted = true;
     if (!this.closureForm.valid) {
       return false;
     } else {
-      if (window.confirm('Are you sure?')) {
+        this.calculateBill();
+        for (const vehicle of this.rentedVehicles) {
+          vehicle.status = this.VehicleStatus[0];
+          vehicle.miles = this.closureForm.value.miles;
+          // @ts-ignore
+          this.apiService.updateVehicle(vehicle._id, vehicle);
+        }
         const id = this.actRoute.snapshot.paramMap.get('id');
+
         this.apiService.deleteRental(id);
-      }
     }
   }
 
